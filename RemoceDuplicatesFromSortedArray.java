6bbcc1626db6d65f16e5d567afe09f3b3ce0075a
marvin <lingwei0604@gmail.com>
2017-10-17 23:20:16 +0800

    add remove duplicates

---
+++ RemoceDuplicatesFromSortedArray.java
@@ -0,0 +1,20 @@
+public class Solution{
+
+
+	public int removeDuplication(int[] nums){
+
+		if(nums == null) return -1;
+
+		if(nums.length <= 1) return nums.length;
+
+		int newIndex = 0;
+
+		for(int i = 1; i < nums.length; i++){
+			if(nums[i] != nums[newIndex]){
+				newIndex++;
+				nums[newIndex] = nums[i];
+			}
+		}
+		return newIndex + 1;
+	}
+}
